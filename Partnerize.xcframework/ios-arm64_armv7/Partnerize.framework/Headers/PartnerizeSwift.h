16eaeb7fbcb59997d7dd1213c15f0b2df2958e9c
Joshua Greenwood <joshua.greenwood@partnerize.com>
2022-05-17 16:01:49 +0100

    Updating files for 2.0.0

---
+++ Partnerize.xcframework/ios-arm64_armv7/Partnerize.framework/Headers/PartnerizeSwift.h
@@ -0,0 +1,19 @@
+//
+//  Partnerize_Swift.h
+//  Partnerize-Swift
+//
+//  Created by Joshua Greenwood on 25/10/2021.
+//  Copyright © 2021 Performance Horizon Group LTD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Partnerize_Swift.
+FOUNDATION_EXPORT double Partnerize_SwiftVersionNumber;
+
+//! Project version string for Partnerize_Swift.
+FOUNDATION_EXPORT const unsigned char Partnerize_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Partnerize_Swift/PublicHeader.h>
+
+
